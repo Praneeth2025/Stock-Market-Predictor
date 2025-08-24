8b76fb4acc8e83628233b08e5373607c83950bda
praneeth2025 <vamsipraneeth2004@gmail.com>
2025-08-24 11:57:46 +0530

    Added requirements

--- project_root/notebooks/streamlit.py
+++ project_root/notebooks/streamlit.py
@@ -2,7 +2,6 @@ import streamlit as st
 import yfinance as yf
 import pandas as pd
 import plotly.express as px
-import pandas_ta.momentum.squeeze_pro as sp
 
 import pickle
 from datetime import datetime
